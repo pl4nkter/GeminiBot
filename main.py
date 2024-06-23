ca1e2527eb4d4dea4401394761b694f107e16331
v3ntages <92279825+v3ntages@users.noreply.github.com>
2024-06-23 18:27:14 +0300

    Update main

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import Final
 import discord
 import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
-from discord import Intents, Client
+from discord import Intents, Client, app_commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,6 +13,9 @@ GOOGLE_API_KEY: Final[str] = os.getenv('GOOGLE_API_KEY')
 DISCORD_TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
 genai.configure(api_key=GOOGLE_API_KEY)
+
+GUILD = discord.Object(id=1254176270871298118)
+default_system_instructions = "You are a teenager that uses Gen Z slang and doesn't capitalize their sentences."
 
 
 class MyClient(Client):
@@ -28,7 +31,14 @@ class MyClient(Client):
             HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
             HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
         })
-        self.chats = {}
+        self.chats: dict[int: list[str]] = {}
+        self.system_instructions: str = default_system_instructions
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=GUILD)
+        await self.tree.sync(guild=GUILD)
 
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
@@ -40,7 +50,7 @@ class MyClient(Client):
         if author.bot or author.system:
             return
 
-        if channel.id != 1254211789605044304:
+        if channel.id != 1254201882692419665:
             return
 
         if not self.chats.get(author.id):
@@ -49,10 +59,8 @@ class MyClient(Client):
         chat = self.chats.get(author.id)
 
         try:
-            response = chat.send_message(["system instructions: you are very romantic and you only speak turkish.",
-                                          f"input: {message.content}", "output: "])
-
-            # response = chat.send_message(message.content)
+            prompt = [f"{self.system_instructions}", f"input: {message.content}", "output: "]
+            response = chat.send_message(prompt)
 
             await message.reply(response.text)
         except Exception as e:
@@ -60,11 +68,42 @@ class MyClient(Client):
 
 
 intents: Intents = Intents.default()
-intents.message_content = True  # NOQA
+intents.message_content = True
 
 
 def main():
     client = MyClient()
+
+    @client.tree.command()
+    async def get_system_instructions(interaction: discord.Interaction):
+        """Clears the message history"""
+        await interaction.response.send_message(client.system_instructions)
+
+    @client.tree.command()
+    async def change_system_instructions(interaction: discord.Interaction, system_instructions: str):
+        """Changes the system instructions.
+
+        Parameters
+        -----------
+        system_instructions: str
+            The system instructions
+        """
+
+        client.system_instructions = system_instructions
+        await interaction.response.send_message('Changed the system instructions to "' + system_instructions + '"')
+
+    @client.tree.command()
+    async def reset_system_instructions(interaction: discord.Interaction):
+        """Resets the system instructions"""
+        client.system_instructions = default_system_instructions
+        await interaction.response.send_message('Reset the system instructions to default')
+
+    @client.tree.command()
+    async def clear(interaction: discord.Interaction):
+        """Clears the chat history"""
+        client.chats = {}
+        await interaction.response.send_message('Cleared the chat history')
+
     client.run(DISCORD_TOKEN)
 
 
