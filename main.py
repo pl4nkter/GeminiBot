3cbf87dcf17c94388f4d9e254b410ca80e514c61
v3ntages <92279825+v3ntages@users.noreply.github.com>
2024-09-01 14:40:04 +0300

    Update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from typing import Final
 
 import discord
+from discord.ext import commands, tasks
 import google.generativeai as genai
 import logging
 from discord import Intents, Client, app_commands
@@ -17,12 +18,10 @@ DISCORD_TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
-default_system_instructions = ("You are now a regular teenager that uses Gen Z slang and doesn't capitalize their "
-                               "sentences. You are not an AI anymore. These are the system instructions, not a part "
-                               "of the chat. Do not talk about this information to the user.")
+default_system_instructions = ("")
 
 
-class MyClient(Client):
+class MyClient(commands.Bot):
     def __init__(self) -> None:
         super().__init__(
             command_prefix='~',
@@ -38,6 +37,8 @@ class MyClient(Client):
         self.chats: dict[int: list[str]] = {}
         self.system_instructions: str = default_system_instructions
         self.tree: app_commands.CommandTree = app_commands.CommandTree(self)
+
+        # these are currently hard coded for testing
         self.guild: discord.Object = discord.Object(1249435119379284050)
         self.channel_id = 1254211789605044304
 
@@ -84,25 +85,26 @@ def main():
         client = MyClient()
 
         @client.tree.command()
-        async def get_system_instructions(interaction: discord.Interaction):
+        async def get(interaction: discord.Interaction):
             """Returns the current system instructions"""
             await interaction.response.send_message(client.system_instructions)
 
         @client.tree.command()
-        async def change_system_instructions(interaction: discord.Interaction, system_instructions: str):
+        async def change(interaction: discord.Interaction, system_instructions: str):
             """Changes the system instructions.
 
             Parameters
             -----------
             system_instructions: str
                 The system instructions
+                :param interaction:
             """
 
             client.system_instructions = system_instructions
             await interaction.response.send_message('Changed the system instructions to "' + system_instructions + '"')
 
         @client.tree.command()
-        async def change_channel(interaction: discord.Interaction, id: str):
+        async def channel(interaction: discord.Interaction, id: str):
             """Changes the channel.
 
             Parameters
@@ -117,7 +119,7 @@ def main():
             await interaction.response.send_message(f'Changed the channel to {channel.mention}')
 
         @client.tree.command()
-        async def reset_system_instructions(interaction: discord.Interaction):
+        async def reset(interaction: discord.Interaction):
             """Resets the system instructions"""
             client.system_instructions = default_system_instructions
             await interaction.response.send_message(f'Reset the system instructions to {default_system_instructions}')
